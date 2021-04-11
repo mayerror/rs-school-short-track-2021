9320e8bf78d72f44cbd7cacd43427a66431bdaeb
mayerror <mayerror@gmail.com>
2021-04-11 18:28:58 +0300

    feat: implement minesweeper function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,48 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const resultMatrix = [];
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    resultMatrix[i] = new Array(matrix[0].length);
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === true) {
+        resultMatrix[i][j] = 1;
+      } else {
+        sum = 0;
+        if (i >= 1 && j >= 1 && matrix[i - 1][j - 1] === true) {
+          sum++;
+        }
+        if (i >= 1 && matrix[i - 1][j] === true) {
+          sum++;
+        }
+        if (i >= 1 && j + 1 < matrix[0].length && matrix[i - 1][j + 1] === true) {
+          sum++;
+        }
+        if (j + 1 < matrix[0].length && matrix[i][j + 1] === true) {
+          sum++;
+        }
+        if (i + 1 < matrix.length && j + 1 < matrix[0].length && matrix[i + 1][j + 1] === true) {
+          sum++;
+        }
+        if (i + 1 < matrix.length && matrix[i + 1][j] === true) {
+          sum++;
+        }
+        if (i + 1 < matrix.length && j >= 1 && matrix[i + 1][j - 1] === true) {
+          sum++;
+        }
+        if (j >= 1 && matrix[i][j - 1] === true) {
+          sum++;
+        }
+        resultMatrix[i][j] = sum;
+      }
+    }
+  }
+  return resultMatrix;
 }
+
 
 module.exports = minesweeper;
