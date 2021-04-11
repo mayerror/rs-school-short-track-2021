781cfe8f1aee2bc5a35753d6adef225cbcf01404
mayerror <mayerror@gmail.com>
2021-04-11 18:56:22 +0300

    feat: implement delete-digit function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -64,5 +64,4 @@ function minesweeper(matrix) {
   return resultMatrix;
 }
 
-
 module.exports = minesweeper;
--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,25 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const string = `${n}`;
+  let arr = [];
+  let min = 0;
+  arr = string.split('');
+  min = arr.reduce((accumulator, currentValue) => {
+    if (currentValue < accumulator) {
+      return currentValue;
+    } return accumulator;
+  }, 9);
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === min) {
+      arr.splice(i, 1);
+      break;
+    }
+  }
+  return Number(arr.join(''));
 }
+
+// console.log(deleteDigit(152));
 
 module.exports = deleteDigit;
