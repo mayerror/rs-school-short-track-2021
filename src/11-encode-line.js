422381a376db971421b36c98d6539664b43efac0
mayerror <mayerror@gmail.com>
2021-04-12 21:14:47 +0300

    feat: implement encode-line function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 0;
+  const arr = (`${str}]`).split('');
+  const res = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      if (count === 0) {
+        count = 2;
+      } else {
+        count++;
+      }
+    } else if (count === 0) {
+      res.push(arr[i - 1]);
+    } else {
+      res.push(count + arr[i - 1]);
+      count = 0;
+    }
+  }
+  return res.join('');
 }
 
 module.exports = encodeLine;
